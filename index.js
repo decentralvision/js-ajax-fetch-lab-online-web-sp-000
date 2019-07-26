0e84c4454d28c27b914f5a31ffd880da39af5b74
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-26 23:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const user = 'decentralvision';
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  // return '62e672ebf155e2c6cd4fb0ca610566bf766e15a0';
-  return ''
+  return '62e672ebf155e2c6cd4fb0ca610566bf766e15a0';
+  // return ''
 }
 
 function forkRepo() {
