230124a08ca44c8c9f7483836965a47328d2dd06
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-27 00:13:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,39 @@ function forkRepo() {
 }
 
 function showResults(json) {
-  console.log(json)
-  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
+    json.html_url}</a>`
 }
 
 function createIssue() {
+  const repo = `${user}/js-ajax-fetch-lab`
+  const url = `${baseURL}/repos/${repo}/issues`
+  const postData = {
+    title: document.getElementById('title').value
+    body: document.getElementById('body').value
+  }
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization : `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => getIssues())
   //use this function to create an issue based on the values input in index.html
 }
 
 function getIssues() {
+  const repo = `${user}/js-ajax-fetch-lab`
+  const url = `${baseURL}/repos/${repo}/issues`
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
   //once an issue is submitted, fetch all open issues to see the issues you are creating
 }
